8b09315547e67d4e18867fe6e6afa8dabb2dd377
Lukas Kraft <69203755+ebaynissen@users.noreply.github.com>
2024-08-16 18:04:01 +0200

    🔖 Github Pages Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/wow-classic-generator",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
